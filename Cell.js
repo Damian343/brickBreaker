b1db3da95002e565d8668013f0282f2796645aa5
damian343 <damianhamilton2@gmail.com>
2018-05-01 11:32:58 -0700

    implemented floodfill to change surrounding blocks to white

--- Cell.js
+++ Cell.js
@@ -1,4 +1,4 @@
-function Cell(i, j, col){
+function Cell(i, j, col) {
         //the index in grid
         this.i = i;
         this.j = j;
@@ -9,13 +9,84 @@ function Cell(i, j, col){
         //color of cell
         this.col = col;
         this.white = false;
-        this.neighCount = 0;
 }
 
 Cell.prototype.show = function() {
     stroke(0);
-    fill(255);
+    if(this.white) {
+      fill(255);
+    } else {
+      fill(this.col);
+    }
     rect(this.x, this.y, w, w);
-//   fill(col);
-//   rect(this.x, this.y, w, w);
+}
+
+Cell.prototype.destroy = function() {
+  this.white = true;
+  this.moveDown();
+  //this.floodFill();
+}
+
+ Cell.prototype.moveDown = function() {
+   for(var y = this.j; y >= 0; y--) {
+     this.swap(grid[this.i][y]);
+   }
+ }
+
+ Cell.prototype.swap = function(neighbor) {
+   this.col = neighbor.col;
+   this.white = false;
+   neighbor.white = true;
+ }
+
+Cell.prototype.contains = function(x, y) {
+  return (x > this.x && x < this.x + w && y > this.y && y < this.y + w);
+}
+
+Cell.prototype.getNeighbors = function() {
+    var neighbors = [];
+    var top    = grid[this.i  ][this.j-1];
+    var right  = grid[this.i+1][this.j  ];
+    var bottom = grid[this.i  ][this.j+1];
+    var left   = grid[this.i-1][this.j  ];
+
+    if(this.safeIndex(top))   { neighbors.push(top)   };
+    if(this.safeIndex(right)) { neighbors.push(right) };
+    if(this.safeIndex(bottom)){ neighbors.push(bottom)};
+    if(this.safeIndex(left))  { neighbors.push(left)  };
+
+    return neighbors;
+}
+
+Cell.prototype.safeIndex = function(cell){
+    if(cell){
+        if(cell.i < cols && cell.i >= 0 && cell.j < rows && cell.j >= 0) { 
+          return true;
+        }
+  } else { return false; }
+}
+
+Cell.prototype.floodFill = function() {
+    var neighbors = this.getNeighbors();
+    if (neighbors.length == 0) { 
+        console.log("no neighbors");
+    } else {
+        for (var i = 0; i < neighbors.length; i++){
+            if(neighbors[i].col == this.col && !neighbors[i].white){
+                neighbors[i].destroy();
+            }
+        }
+    }
+    // for (var xoff = -1; xoff <= 1; xoff++) {
+    //     for ( var yoff = -1; yoff <= 1; yoff++) {
+    //       var i = this.i + xoff;
+    //       var j = this.j + yoff;
+    //       if (i > -1 && i < cols && j > -1 && j < rows) {
+    //         var neighbor = grid[i][j];
+    //         if(neighbor.col == this.col && !neighbor.white) {
+    //           neighbor.destroy();
+    //         }
+    //       }
+    //     }
+    // }
 }
--- brickBreaker.js
+++ brickBreaker.js
@@ -3,30 +3,31 @@ var cols;
 var rows;
 var totalCells = 0;
 //size of each cell
-var w = 40;
+var w = 50;
 var colors;
 
 function setup() {
-	createCanvas(401, 601);
-	cols = floor(width / w);
-	rows = floor(height / w);
-	grid = make2DArray(cols,rows);
+  createCanvas(401, 601);
+  cols = floor(width / w);
+  rows = floor(height / w);
+  grid = make2DArray(cols, rows);
 
-	colors = [red, green, blue];
-
-	for(var i = 0; i < cols; i++){
-		for(var j = 0; j < rows; j++){
-			grid[i][j] = new Cell(i, j, colors[random(4)]);
-			totalCells++;
-		}
-	}
+  colors = [color(255, 0, 0), color(0, 255, 0), color(0, 0, 255), color(255, 255, 0)];
+  
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      var col = colors[floor(random(colors.length))];
+      grid[i][j] = new Cell(i, j, col);
+      totalCells++;
+    }
+  }
 }
 
 function mousePressed(){
 	for(var i = 0; i < cols; i++){
 		for(var j = 0; j < rows; j++){
             if(grid[i][j].contains(mouseX, mouseY)){
-                grid[i][j].reveal();
+                grid[i][j].destroy();
             }       
         }
     }
@@ -40,11 +41,10 @@ function make2DArray(cols, rows) {
   return arr;
 }
 
-
 function draw() {
-    for(var i = 0; i < cols; i++){
-		for(var j = 0; j < rows; j++){
-            grid[i][j].show();
-        }
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      grid[i][j].show();
     }
+  }
 }
